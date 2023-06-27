6c2355d853b0cfc74e65a0b456e0ae6ccd03b7e7
Stephen Ruhlen <44741605+ruhlens@users.noreply.github.com>
2023-06-26 20:44:38 -0400

    Add files via upload

---
+++ FinnHubData.py
@@ -0,0 +1,50 @@
+import finnhub
+
+class FinnHubData:
+
+    def __init__(self, key):
+        self.key = key
+        self.client = finnhub.Client(api_key=self.key)
+
+    '''
+    Returns candlestick data for the given ticker
+    Args:
+        ticker(str): the given ticker
+        resolution(str): the timeframe for the data (1, 5, 15, 30, 60, D, W, M)
+        start(str): unix timestamp for start time 
+        end(str): unix timestamp for end time
+    Returns:
+        self.client.stock_candles(ticker, resolution)
+    '''
+    def getCandle(self, ticker, resolution, start, end):
+        return self.client.stock_candles(ticker, resolution)
+
+    def getPrice(self, ticker):
+        return self.client.quote(ticker).get('c')
+
+    def getEMA(self, ticker, timeperiod=3, seriestype='c'):
+        #this line is broken fix it
+        return self.client.technical_indicator(symbol=ticker, indicator='ema', timeperiod=3, seriestype='c')
+
+    '''
+    Returns data for a given ticker containing the MACD correlating to the args specified in this format:
+    {'c': [], 'h': [], 'l': [], 'o': [], 's': '', 'macd': [], 't': [], 'v': []}
+    Args:
+        ticker
+        start
+        end
+        res
+        stype
+        fastperiod
+        slowperiod
+        signal_period
+    Returns:
+        self.client.technical_indicator(symbol=ticker, indicator= 'macd', resolution=res, _from=start, to=end, indicator_fields={'seriestype': stype,
+                                                                                                   'fastperiod': fastperiod, 'slowperiod': slowperiod,
+                                                                                                   'signalperiod': signalperiod})
+    '''
+    def getMACD(self, ticker, start, end, res='D', stype='c', fastperiod=12, slowperiod=26, signalperiod=9):
+        return self.client.technical_indicator(symbol=ticker, indicator= 'macd', resolution=res, _from=start, to=end, indicator_fields={'seriestype': stype,
+                                                                                                   'fastperiod': fastperiod, 'slowperiod': slowperiod,
+                                                                                                   'signalperiod': signalperiod})
+
---
+++ MACDAlgorithm.py
@@ -0,0 +1,72 @@
+"""
+THIS IS A PAPER TRADING PROGRAM BASED ON THE RSI ALGORITHM
+BUY AT RSI = 30
+SELL AT RSI = 70
+"""
+
+from algorithms import Algorithm
+from graph import Graph
+import keyboard
+from datetime import datetime
+from FinnHubData import FinnHubData
+import time
+
+#get data collection function running on a separate thread
+
+def unixTime(days):
+    return days * 86400
+
+def main():
+    """Main function to handle the algorithm."""
+    print("RSI Trading Algorithm Started. Press 'Q' to Close")
+    run = True
+    money = 100000
+    shares = 0
+    x = []
+    y = []
+    index = 0
+    graph = Graph()
+    buys = []
+    sells = []
+    avBuyPrice = 0
+    avSellPrice = 0
+    percentage = 0
+    key = 'cc06epiad3idf21ispa0'
+    while run:
+        if keyboard.is_pressed("q"):
+            run = False
+        ticker = "AAPL"
+        client = FinnHubData(key)
+        call = client.getMACD(ticker, int(time.time() - unixTime(365)), int(time.time()))
+        time.sleep(1)
+        currentPrice = call.get('c')[-1]
+        currentTime = datetime.now().time().strftime("%H:%M:%S")
+        indicator = call.get('macd')[-1]
+        x.append(float(f'{currentPrice:.2f}'))
+        y.append(currentTime)
+        if len(x) > 20:
+            x.remove(x[0])
+            graph.clear()
+        if len(y) > 20:
+            y.remove(y[0])
+            graph.clear()
+        index += 1
+        algo = Algorithm(indicator).MACD(indicator, 0, 0, currentPrice, money, shares)
+        orderType = algo[0]
+        money = algo[1]
+        shares = algo[2]
+        price = algo[3]
+        percentage = (money - 100000) / 100000
+        graph.liveGraph(y,x, buys, avBuyPrice, avSellPrice, percentage, money)
+        if orderType == "B":
+            buys.append(price)
+        if orderType == "S":
+            sells.append(price)
+        if len(buys) > 0:
+            avBuyPrice = sum(buys) / len(buys)
+        if len(sells) > 0:
+            avSellPrice = sum(sells) / len(sells)
+    graph.show()
+
+if __name__ == "__main__":
+    main()
